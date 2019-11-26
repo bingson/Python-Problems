cc19a4ae03ab9282bd41367af480e25cdd1c291a
Bingson Huang <bingson@gmail.com>
2019-11-26 15:25:02 -0500

    Add files via upload

---
+++ Hackerrank_Problems/1_ARRAYS_Array_Manipulation.py
@@ -0,0 +1,52 @@
+# n,queries:     5 [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from typing import List 
+
+# Complete the arrayManipulation function below.
+def arrayManipulation(n: int, queries: List[int]) -> int:
+    max_val: int = 0
+
+    # (1) init array of zeroes 
+    b_arr: list = n * [0]
+    
+    # loop to  update array and max_val
+    for _,q in enumerate(queries):
+        for i,e in enumerate(b_arr[q[0]-1:q[1]]):
+            
+            # (2) perform operations
+            b_arr[q[0]+i-1] += q[2]
+    
+            # (3) update max_val
+            if b_arr[q[0]-1+i] > max_val:
+                max_val = b_arr[q[0]-1+i]
+            
+        # print('b_arr {0}:\t'.format(_), b_arr)
+    
+    # print('b_arr_final:\t', b_arr)
+    return max_val
+
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+    fptr.write(str(result) + '\n')
+    fptr.close()
